29ec201ca9b0a04eaafbb3b24d710a5223f9d0af
vineetha-batchu <77755310+vineetha-batchu@users.noreply.github.com>
2021-10-28 21:37:12 -0500

    added input restrictions for covid pediction screen

--- app/src/main/java/com/example/group2_project/CovidPrediction.java
+++ app/src/main/java/com/example/group2_project/CovidPrediction.java
@@ -2,12 +2,22 @@ package com.example.group2_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CovidPrediction extends AppCompatActivity {
+    static int countIndex=0;
+    Button btn_predict;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_covid_prediction);
+        btn_predict=findViewById(R.id.btnPredict);
     }
+    public void predict_method(View v){
+
+
+    }
+
 }
